86bfb063947ca0f06e01b395a93aee80f0f00204
Abigail Harrison <abigail@giffgaff.co.uk>
2022-07-14 20:09:28 +0100

    wip: Begin Strapi setup

--- backend/src/api/article/services/article.js
+++ backend/src/api/article/services/article.js
@@ -1,9 +1,40 @@
-'use strict';
+module.exports = {
+  postsReport: async () => {
+    try {
+      // fetching data
+      const entries = await strapi.entityService.findMany(
+        "api::article.article",
+        {
+          fields: ["id", "title", "author", "createdAt", "content"],
+          populate: {
+            author: {
+              fields: ["name", "avatar"],
+            },
+          },
+        }
+      );
 
-/**
- * article service.
- */
+      // reduce the data to the format we want to return
+      let entriesReduced;
+      if (entries && Array.isArray(entries)) {
+        entriesReduced = entries.reduce((acc, item) => {
+          acc = acc || [];
+          acc.push({
+            id: item.id,
+            title: item.title || "",
+            category: item.category.name || "",
+            publishedDate: new Date(item.createdAt).toDateString() || "",
+            authorName: item.author?.name || "",
+            authorAvatar: item.author?.avatar || "",
+          });
+          return acc;
+        }, []);
+      }
 
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::article.article');
+      // return the reduced data
+      return entriesReduced;
+    } catch (err) {
+      return err;
+    }
+  },
+};
