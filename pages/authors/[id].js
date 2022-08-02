e960af45910c84224322fe3dcda349c9e079661b
Abigail Harrison <abigail@giffgaff.co.uk>
2022-08-02 15:18:38 +0100

    wip: Update styling on single author page

--- pages/authors/[id].js
+++ pages/authors/[id].js
@@ -1,5 +1,6 @@
 import { useRouter } from 'next/router';
 import Image from 'next/image';
+import { FaGithub, FaLinkedin } from 'react-icons/fa';
 import Layout from '@/components/layout';
 import styles from '@/styles/SinglePages.module.css';
 import contentSvc from '@/services/content-svc';
@@ -28,13 +29,28 @@ export default function SingleAuthor(author) {
 					<h2 className={styles.title}>{author.name}</h2>
 					<p className={styles.tagline}>{author.tagline}</p>
 					<hr className={styles.hr} />
-					<div dangerouslySetInnerHTML={{ __html: aboutYou }} />
-					<a href={author.github} target="_blank" rel="noopener noreferrer">
-						GitHub
-					</a>
-					<a href={author.linkedin} target="_blank" rel="noopener noreferrer">
-						LinkedIn
-					</a>
+					<div
+						dangerouslySetInnerHTML={{ __html: aboutYou }}
+						className={styles.aboutYou}
+					/>
+					<div className={styles.socialGrid}>
+						<a
+							href={author.github}
+							target="_blank"
+							rel="noopener noreferrer"
+							className={styles.socialLinks}
+						>
+							<FaGithub className={styles.icon} /> GitHub
+						</a>
+						<a
+							href={author.linkedin}
+							target="_blank"
+							rel="noopener noreferrer"
+							className={styles.socialLinks}
+						>
+							<FaLinkedin className={styles.icon} /> LinkedIn
+						</a>
+					</div>
 					<div className={styles.avatarContainer}>
 						<Image
 							src={author.image.formats.small.url}
